27d9ba12076e27de31d25ae233f6b8902dec6f84
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-28 21:06:42 -0300

    Creamos la entrada inicial que levanta el servidor de node

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello')
+import dotenv from 'dotenv';
+import express from 'express';
+
+import 'reflect-metadata';
+
+dotenv.config();
+
+function main() {
+  const port = process.env.SERVER_PORT;
+
+  const app = express();
+
+  app.get('/', (req, res) => {
+    return res.json({ message: 'Health Check' });
+  });
+
+  app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+  });
+}
+
+main();
